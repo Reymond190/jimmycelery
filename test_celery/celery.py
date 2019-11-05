4f93ff70dde17fb7290e6b41d70ba0d477db93d3
reymond <josephreymond2011@gmail.com>
2019-11-05 12:59:22 +0530

    fresh1

--- test_celery/celery.py
+++ test_celery/celery.py
@@ -2,6 +2,6 @@ from __future__ import absolute_import
 from celery import Celery
 
 app = Celery('test_celery',
-             broker='amqp://jimmy:jimmy123@localhost/jimmy_vhost',
+             broker='amqp://reymond201:reymond201@localhost/ray_vhost',
              backend='rpc://',
              include=['test_celery.tasks'])
